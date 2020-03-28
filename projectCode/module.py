c92376e7770a1ca1a186db090156bc9bcc21c4d0
Tanay Agrawal <tanayagrawal@tanays-mbp.home>
2020-03-28 18:43:52 -0400

    adding world class and basic setup code

---
+++ projectCode/module.py
@@ -0,0 +1,58 @@
+class World():
+    """
+    Class for handling the map 
+    functions and variables
+    """
+    def __init__(self, worldJson):
+        """
+        Initializes all the variables
+        that are part of the class Map
+        """
+        self.world_json = worldJson
+        
+    def get_canvas_size(self):
+        """
+        Function that returns the size of canvas from Json
+        return: array having width and height
+        """
+        return world_json["canvas_size"]
+    
+    def set_background_color(self):
+        """
+        Sets the background color
+        """
+        background(self.world_json["canvas_color"])
+        
+    def draw_all_obstacles(self):
+        """
+        Function that creates all the obstacles on
+        the map
+        """
+        obstaclesJson = self.world_json["obstacles"]
+        for obstacle in obstaclesJson.keys():
+            tempObstacleJson = obstaclesJson[obstacle]
+            self.draw_obstacle_utility(tempObstacleJson, obstacle)
+        
+    def draw_obstacle_utility(self, locationList, obstacle):
+        """
+        Function that creates the obstacle on
+        the map
+        loationList: dictionary of all the locations
+                     where the obstacles are to be drawn
+        obstacle: name of the obstacle that is being drawn
+        """
+        totalVertices = len(locationList)
+        fill(0)
+        stroke(0)
+        strokeWeight(2)
+        beginShape()
+        for i in range(0,totalVertices):
+            location = locationList[i]
+            vertex(location[0],location[1])
+        endShape(CLOSE)
+        
+    def draw_bot(self):
+        """
+        Function draws the bot on the map
+        """
+        circle(self.world_json["bot_start"][0], self.world_json["bot_start"][1], 10)
--- projectCode/projectCode.pyde
+++ projectCode/projectCode.pyde
@@ -1,1 +1,41 @@
-print "hello"
+import json
+from module import World
+
+def readJson(filename):
+    """
+    Reads the file and returns file content
+    filename: name of the file to be read
+    return: file content in string
+    """
+    with open(filename) as fp:
+        data = fp.read()
+    return data
+
+def loadJson(data):
+    """
+    Loads the file content in to json object
+    data: data to be loaded into json object
+    return: json object of data
+    """
+    try:
+        data = json.loads(data)
+        return data
+    except Exception as e:
+        print "Error loading json: {}".format(str(e))
+
+world = None
+def setup():
+    """
+    setup function for the game
+    """
+    global world
+    size(640, 480)
+    fileData = readJson('map.json')
+    worldJson = loadJson(fileData)
+    world = World(worldJson)
+    gameSize = world.set_background_color()
+    world.draw_all_obstacles()
+    world.draw_bot()
+    
+def draw():
+    world.draw_bot()
